a70cc65f984af2df565b2c0dd0d74477561b7614
emil720a1 <emil720apro@gmail.com>
2024-09-21 22:21:09 +0300

    Update Lab1.cpp

--- Lab1.cpp
+++ Lab1.cpp
@@ -1,4 +1,4 @@
-      #include <iostream>
+       #include <iostream>
     using namespace std;
     int main(){
     int A;
@@ -6,20 +6,21 @@
     cin>>A;
 
 
-    if(A>89&&A<101){
-    cout<<"A";   
-    } else if(A>81&&A<89){
+    if(A>=89&&A<101){
+    cout<<"A";
+    
+    } else if(A>=81&&A<=89){
     cout<<"B";
-    } else if(A>74&&A<81){
+    } else if(A>=74&&A<81){
         cout<<"C";
-    } else if(A>66&&A<74){
+    } else if(A>=66&&A<74){
         cout<<"D";
-    } else if(A>59&&A<66){
+    } else if(A>=59&&A<66){
         cout<<"E";
-    } else if(A>34&&A<59){
+    } else if(A>=34&&A<59){
         cout<<"FX";
-    } else if(A>1&&A<34){
+    } else if(A>=1&&A<34){
         cout<<"F";
 
-    }
+    } else {cout<< "num";}
     }
